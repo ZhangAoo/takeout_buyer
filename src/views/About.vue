d0d832c12b9b91817c3a64f0cbb5a42022a5bd25
ZhangAoo <zhangao0316@163.com>
2020-09-04 11:21:43 +0800

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-wrap">
+    <h1>外卖订餐系统 V2.0</h1>
+    <h2>技术架构 Spring Cloud Alibaba + Vue</h2>
+  </div>
+</template>
+<script>
+  export default {
+    created() {
+      this.$store.state.index = 3
+    }
+  }
+</script>
+
+<style>
+  .page-wrap {
+    overflow: auto;
+    height: 100%;
+    margin-top: 60px;
+  }
+</style>
